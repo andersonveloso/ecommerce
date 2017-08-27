add298347dd59e11cdd92867af9a92eb8a1e52ea
Anderson Veloso <andersonveloso_mg@hotmail.com>
2017-08-26 21:05:34 -0300

    foi criado o CRUD  de usuários

--- index.php
+++ index.php
@@ -15,46 +15,139 @@ $app->config('debug', true);
 $app->get('/', function() {
 
 User::verifyLogin();
-    
-$page = new Page();
+	    
+	$page = new Page();
 
-$page->setTpl("index");	
+	$page->setTpl("index");	
 
 });
 
 $app->get('/admin', function() {
     
-$page = new PageAdmin();
+	$page = new PageAdmin();
 
-$page->setTpl("index");	
+	$page->setTpl("index");	
 
 });
 
 $app->get('/admin/login', function() {
     
-$page = new PageAdmin([
-	"header"=>false,
-	"footer"=>false]);
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+		]);
 
-$page->setTpl("login");	
+	$page->setTpl("login");	
 
 });
 
 $app->post('/admin/login', function() {
     
-User::login($_POST["login"], $_POST["password"]);
+	User::login($_POST["login"], $_POST["password"]);
 
-header("Location: /admin");
+	header("Location: /admin");
 
-exit;
+	exit;
 
 });
 
 $app->get('/admin/logout', function(){
 
-User::logout();
+	User::logout();
 
-header("Location: /admin/login");
+	header("Location: /admin/login");
+	exit;
+
+});
+
+$app->get("/admin/users", function(){
+
+User::verifyLogin();
+
+$users = User::listAll();
+
+$page = new PageAdmin();
+
+$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+$app->get("/admin/users/create", function() {
+
+User::verifyLogin();
+
+$page = new PageAdmin();
+
+$page->setTpl("users-create");
+
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser){
+
+User::verifyLogin();
+
+$user = new User();
+
+$user->get((int)$iduser);
+
+$user->delete();
+
+header("Location: /admin/users");
+exit;
+
+});
+
+$app->get("/admin/users/:iduser", function($iduser) {
+
+User::verifyLogin();
+
+$user = new User();
+
+$user->get((int)$iduser);
+
+$page = new PageAdmin();
+
+$page->setTpl("users-update", array(
+	"user"=>$user->getValues()
+
+	));
+
+});
+
+$app->post("/admin/users/create", function() {
+
+User::verifyLogin();
+
+$user = new User();
+
+$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+$user->setData($_POST);
+
+$user->save();
+
+header("Location: /admin/users");
+exit;
+
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+
+User::verifyLogin();
+
+$users = new User();
+
+$users->get((int)$iduser);
+
+$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+$users->setData($_POST);
+
+$users->update();
+
+header("Location: /admin/users");
 exit;
 
 });
